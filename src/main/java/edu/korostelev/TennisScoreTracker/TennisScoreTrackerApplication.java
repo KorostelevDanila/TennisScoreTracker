98b7eed073441376f9a8aeea495eb98d847fd996
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-03-12 17:45:40 +0300

    initial commit

--- src/main/java/edu/korostelev/TennisScoreTracker/TennisScoreTrackerApplication.java
+++ src/main/java/edu/korostelev/TennisScoreTracker/TennisScoreTrackerApplication.java
@@ -2,8 +2,10 @@ package edu.korostelev.TennisScoreTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages= "edu/korostelev/TennisScoreTracker/models")
 public class TennisScoreTrackerApplication {
 
 	public static void main(String[] args) {
