a020531fb4c708e8130c6b62327f43a4d4eb3d87
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-03-12 17:45:40 +0300

    initial commit

---
+++ src/test/java/edu/korostelev/TennisScoreTracker/TennisScoreTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.korostelev.TennisScoreTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisScoreTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
