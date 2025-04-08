1d3cdc9905ea3ec8ab9221679a53696a2fef204f
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-04-08 16:37:25 +0300

    add MatchesService and PlayersService dependency to MatchScoreController

--- src/main/java/edu/korostelev/TennisScoreTracker/controller/MatchScoreController.java
+++ src/main/java/edu/korostelev/TennisScoreTracker/controller/MatchScoreController.java
@@ -1,6 +1,9 @@
 package edu.korostelev.TennisScoreTracker.controller;
 
 import edu.korostelev.TennisScoreTracker.model.CurrentMatch;
+import edu.korostelev.TennisScoreTracker.model.Player;
+import edu.korostelev.TennisScoreTracker.service.MatchesService;
+import edu.korostelev.TennisScoreTracker.service.PlayersService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
@@ -8,15 +11,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Optional;
+
 @Controller
 @RequestMapping("/match-score")
 public class MatchScoreController {
+    private final MatchesService matchesService;
+    private final PlayersService playersService;
+
+    public MatchScoreController(MatchesService matchesService, PlayersService playersService) {
+        this.matchesService = matchesService;
+        this.playersService = playersService;
+    }
+
     @GetMapping
     public String getMatchScore(
             @RequestParam("uuid") Integer uuid,
             ModelMap modelMap
     ) {
         CurrentMatch currentMatch = new CurrentMatch(1, 2);
+        CurrentMatch currentMatch = matchesService.getCurrentMatch(uuid);
 
         modelMap.addAttribute("uuid", uuid);
         modelMap.addAttribute("match", currentMatch);
