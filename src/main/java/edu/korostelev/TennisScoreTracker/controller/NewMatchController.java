c6b691027036c22a65e67caf741a4099483f4039
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-04-08 15:34:26 +0300

    create NewMatchController

---
+++ src/main/java/edu/korostelev/TennisScoreTracker/controller/NewMatchController.java
@@ -0,0 +1,36 @@
+package edu.korostelev.TennisScoreTracker.controller;
+
+import edu.korostelev.TennisScoreTracker.service.MatchesService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+@RequestMapping("/new-match")
+public class NewMatchController {
+    private final MatchesService matchesService;
+
+    public NewMatchController(MatchesService matchesService) {
+        this.matchesService = matchesService;
+    }
+
+    @GetMapping
+    public String createMatch() {
+        return "new-match";
+    }
+
+    @PostMapping
+    public String startMatch(
+            @RequestParam String firstPlayerName,
+            @RequestParam String secondPlayerName,
+            RedirectAttributes redirectAttributes
+    ) {
+        Integer createdMatchId = matchesService.createMatch(firstPlayerName, secondPlayerName);
+        redirectAttributes.addAttribute("uuid", createdMatchId);
+        return "redirect:/match-score";
+    }
+}
