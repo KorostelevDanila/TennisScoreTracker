35a7a9b0445e061d0c04291a2b5903897e1e867b
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-04-08 16:38:23 +0300

    implement searching and creating players in databes in MatchesService

--- src/main/java/edu/korostelev/TennisScoreTracker/service/MatchesService.java
+++ src/main/java/edu/korostelev/TennisScoreTracker/service/MatchesService.java
@@ -1,24 +1,39 @@
 package edu.korostelev.TennisScoreTracker.service;
 
 import edu.korostelev.TennisScoreTracker.model.CurrentMatch;
+import edu.korostelev.TennisScoreTracker.model.Player;
+import edu.korostelev.TennisScoreTracker.repository.PlayersRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.Optional;
 
 @Service
 public class MatchesService {
     private final HashMap<Integer, CurrentMatch> matches = new HashMap<>();
 
-    //TODO implement searching and creating players in database
+    private final PlayersService playersService;
+
+    public MatchesService(PlayersService playersService) {
+        this.playersService = playersService;
+    }
+
     //TODO implement generation of unique id for matches
     public Integer createMatch(String firstPlayerName, String secondPlayerName) {
-        int firstPlayerId = 1;
-        int secondPlayerId = 2;
+        Optional<Player> firstPlayer = playersService.findByName(firstPlayerName);
+        Optional<Player> secondPlayer = playersService.findByName(secondPlayerName);
+
+        if (firstPlayer.isEmpty()) {
+            firstPlayer = playersService.savePlayer(firstPlayerName);
+        }
+        if (secondPlayer.isEmpty()) {
+            secondPlayer = playersService.savePlayer(secondPlayerName);
+        }
 
         int matchId = 0;
 
         matches.put(matchId,
-                new CurrentMatch(firstPlayerId, secondPlayerId)
+                new CurrentMatch(firstPlayer.get().getId(), secondPlayer.get().getId())
         );
 
         return matchId;
