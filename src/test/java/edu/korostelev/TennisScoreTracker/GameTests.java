c8927b5986654f3092c6bb6e08b90f8b13a1c873
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-04-17 16:00:08 +0300

    create tests for Game class logic

---
+++ src/test/java/edu/korostelev/TennisScoreTracker/GameTests.java
@@ -0,0 +1,70 @@
+package edu.korostelev.TennisScoreTracker;
+
+import edu.korostelev.TennisScoreTracker.model.Player;
+import edu.korostelev.TennisScoreTracker.model.tennisScoreModels.Game;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameTests {
+    Game testGame;
+    Player firstPlayer;
+    Player secondPlayer;
+
+    final int firstPlayerId = 1;
+    final int secondPlayerId = 2;
+
+    public void init() {
+        firstPlayer = new Player("John");
+        secondPlayer = new Player("Winkie");
+        firstPlayer.setId(firstPlayerId);
+        secondPlayer.setId(secondPlayerId);
+        testGame = new Game(firstPlayer, secondPlayer);
+    }
+
+    @Test
+    public void normalGame() {
+        init();
+        testGame.winnedBy(firstPlayer);
+        testGame.winnedBy(firstPlayer);
+        testGame.winnedBy(firstPlayer);
+        Assertions.assertEquals(firstPlayer, testGame.winnedBy(firstPlayer).get());
+    }
+
+    @Test
+    public void tieGame() {
+        init();
+        testGame.winnedBy(firstPlayer);
+        testGame.winnedBy(firstPlayer);
+        testGame.winnedBy(firstPlayer);
+
+        testGame.winnedBy(secondPlayer);
+        testGame.winnedBy(secondPlayer);
+        testGame.winnedBy(secondPlayer);
+
+        testGame.winnedBy(firstPlayer);
+        Assertions.assertEquals(firstPlayer, testGame.winnedBy(firstPlayer).get());
+    }
+
+    @Test
+    public void prolongedTieGame() {
+        init();
+
+        // first player get up to 40
+        testGame.winnedBy(firstPlayer);
+        testGame.winnedBy(firstPlayer);
+        testGame.winnedBy(firstPlayer);
+
+        // second player get up to 40
+        testGame.winnedBy(secondPlayer); // 15
+        testGame.winnedBy(secondPlayer);
+        testGame.winnedBy(secondPlayer);
+
+        // first and second player get tie after taking advantage
+        testGame.winnedBy(firstPlayer);
+        testGame.winnedBy(secondPlayer);
+
+        // first player gets final advantage
+        testGame.winnedBy(firstPlayer);
+        Assertions.assertEquals(firstPlayer, testGame.winnedBy(firstPlayer).get());
+    }
+}
