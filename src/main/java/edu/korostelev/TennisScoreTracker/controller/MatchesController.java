eed6ab8490ee352f4271e02e4051865c18990c18
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-04-21 16:04:14 +0300

    create get method for /matches mapping

--- src/main/java/edu/korostelev/TennisScoreTracker/controller/MatchesController.java
+++ src/main/java/edu/korostelev/TennisScoreTracker/controller/MatchesController.java
@@ -1,15 +1,29 @@
 package edu.korostelev.TennisScoreTracker.controller;
 
+import edu.korostelev.TennisScoreTracker.model.Match;
+import edu.korostelev.TennisScoreTracker.repository.MatchesRepository;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/matches")
 public class MatchesController {
+    private final MatchesRepository matchesRepository;
+
+    public MatchesController(MatchesRepository matchesRepository) {
+        this.matchesRepository = matchesRepository;
+    }
 
     @GetMapping
-    public String getMatches() {
+    public String getMatches(ModelMap modelMap) {
+        List<Match> allMatches = matchesRepository.findAll();
+
+        modelMap.addAttribute("matches", allMatches);
         return "matches";
     }
 }
