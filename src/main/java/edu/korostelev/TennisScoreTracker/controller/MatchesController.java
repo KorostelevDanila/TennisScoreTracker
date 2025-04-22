d7af8ae5376381a58751bf61d9fb798047da60ab
Korostelev Danila <korostelev.danila2@yandex.ru>
2025-04-22 19:43:27 +0300

    implement pagination for matches page

--- src/main/java/edu/korostelev/TennisScoreTracker/controller/MatchesController.java
+++ src/main/java/edu/korostelev/TennisScoreTracker/controller/MatchesController.java
@@ -2,10 +2,13 @@ package edu.korostelev.TennisScoreTracker.controller;
 
 import edu.korostelev.TennisScoreTracker.model.Match;
 import edu.korostelev.TennisScoreTracker.repository.MatchesRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,10 +22,17 @@ public class MatchesController {
     }
 
     @GetMapping
-    public String getMatches(ModelMap modelMap) {
-        List<Match> allMatches = matchesRepository.findAll();
+    public String getMatches(
+            @RequestParam(required = false, defaultValue = "1") String page,
+            ModelMap modelMap) {
+        Page<Match> allMatches = matchesRepository.findAll(PageRequest.of((Integer.parseInt(page)-1), 5));
 
-        modelMap.addAttribute("matches", allMatches);
+        modelMap.addAttribute("matches", allMatches.getContent());
+        modelMap.addAttribute("currentPage", allMatches.getNumber() + 1);
+        modelMap.addAttribute("hasPrevious", allMatches.hasPrevious());
+        modelMap.addAttribute("hasNext", allMatches.hasNext());
+        modelMap.addAttribute("totalPages", allMatches.getTotalPages());
+
         return "matches";
     }
 }
